7d0995b70da95e8e697537022d727051cc420652
Christopher Raffaele <alumnye@gmail.com>
2021-10-22 17:35:06 -0400

    keypad inputs working and controlled

--- client/app.jsx
+++ client/app.jsx
@@ -1,15 +1,41 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 export default function Calculator(props) {
 
+  const [inputValue, updateInput] = useState('');
+  const [selection, setSelection] = useState({start: 0, end: 0});
+
+  const inputRef = useRef(null);
+
   const handleClick = (e) => {
-    console.log(e.target.id);
+    const cursorPosition = inputRef.current.selectionStart;
+    const selectStart = inputRef.current.selectionStart;
+    const selectEnd = inputRef.current.selectionEnd;
+    const newInput =
+    inputValue.slice(0, cursorPosition) + e.target.id + inputValue.slice(cursorPosition);
+
+    updateInput(newInput);
+    setSelection({start: selectStart, end: selectEnd});
   }
 
   const handleKey = (e) => {
     console.log(e.key);
-
+    inputRef.current.focus();
   }
+
+  const handleChange = (e) => {
+    updateInput(e.target.value);
+  }
+
+  useEffect(() => {
+    console.log('use effect called!')
+    console.log('selection log:', selection);
+    inputRef.current.focus();
+    document.body.addEventListener("keypress", handleKey);
+    //handle seletion coords
+    const {start, end} = selection;
+    inputRef.current.setSelectionRange(start + 1, end + 1);
+  }, [selection])
 
   const inputs = [
     '(',
@@ -36,7 +62,13 @@ export default function Calculator(props) {
 
   return (
     <div className="calc-box">
-      <input type="text" placeholder="0" onKeyPress={handleKey} />
+      <input
+      type="text"
+      placeholder="0"
+      ref={inputRef}
+      onChange={handleChange}
+      value={inputValue}
+      />
       <div className="keypad" onClick={handleClick}>
         {inputs.map((input, i) => (
           <button

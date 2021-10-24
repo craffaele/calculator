fef6c661f413f28f5546a68d1f6feb04d9befc2b
Christopher Raffaele <alumnye@gmail.com>
2021-10-24 01:14:55 -0400

    calculation logic on server handles arithmetic strings without parens

--- server/evaluate.js
+++ server/evaluate.js
@@ -7,21 +7,49 @@ const evaluate = (expression) => {
 
 const stage = (exp) => {
   console.log('solver log', exp);
-  const ops = ['*', '/', '+', '-'];
-  // multiplication > division > addition > subtraction
+
+  const ops = [['*', '/'], ['+', '-']];
+  // (multiplication, division) > (addition, subtraction)
   for (let op of ops) {
-    let opIndex = exp.indexOf(op);
+    console.log('current op set:', op);
+    // let firstIndex = exp.indexOf(op[0]);
+    // let secondIndex = exp.indexOf(op[1]);
+    // console.log('first index:', firstIndex);
+    // console.log('second index:', secondIndex);
+
+    const findNextOperatorIndex = (exp, firstIndex, secondIndex) => {
+      if (firstIndex === secondIndex) {
+        console.log('they are both -1');
+        return -1;
+      } else if (firstIndex === -1 || secondIndex === -1) {
+        console.log('whichever is greater.')
+        return Math.max( exp.indexOf(op[0]), exp.indexOf(op[1]) );
+      } else {
+        console.log('whichever is less.')
+        return Math.min( exp.indexOf(op[0]), exp.indexOf(op[1]) );
+      }
+    }
+    let opIndex = findNextOperatorIndex(exp, exp.indexOf(op[0]), exp.indexOf(op[1]));
+    // let opIndex = Math.min( exp.indexOf(op[0]), exp.indexOf(op[1]) );
+    console.log('opIndex:', opIndex);
+
+    let currentOp = exp[opIndex];
+    console.log('currentOp:', currentOp);
+
     while (opIndex !== -1) {
-      console.log('includes', op);
-      exp = search(exp, op, opIndex);
-      opIndex = exp.indexOf(op);
+      console.log('LOOP RUNS')
+      exp = search(exp, currentOp, opIndex);
+      // opIndex = Math.max( exp.indexOf(op[0]), exp.indexOf(op[1]) );
+      // findNextOperator(exp);
       console.log('mutated exp:', exp);
       // reassign opIndex
-      opIndex = exp.indexOf(op, opIndex);
+      // opIndex = Math.min( exp.indexOf(op[0]), exp.indexOf(op[1]) );
+      opIndex = findNextOperatorIndex(exp, exp.indexOf(op[0]), exp.indexOf(op[1]));
+      console.log('opIndex loop:', opIndex)
+      currentOp = exp[opIndex];
     }
   }
   return exp[0];
-
 }
 
 const search = (exp, op, opIndex) => {
@@ -32,7 +60,6 @@ const search = (exp, op, opIndex) => {
   const trancheResult = solve(firstVal, secondVal, op);
   exp.splice(opIndex - 1, 3, trancheResult);
   return exp;
-
 }
 
 const solve = (firstVal, secondVal, op) => {

61ba77a516308adef338a0d001bf942ea03d27cc
Christopher Raffaele <alumnye@gmail.com>
2021-10-24 14:02:20 -0400

    draft logic for paren evaluation

--- server/evaluate.js
+++ server/evaluate.js
@@ -1,6 +1,36 @@
-const evaluate = (expression) => {
+const evaluate = async (expression) => {
   // const splitExpression = expression.split(/[^0-9\.]+/g);
   const splitExpression = expression.split(/(?=[+\-/*()])|(?<=[+\-/*()])/g);
+
+  // another function to check parens called here
+  const evaluateParens = async (exp) => {
+    console.log('eval parens called');
+    let openingParen = exp.indexOf('(');
+    if (openingParen === -1) return exp;
+
+    let i = openingParen;
+    let closingParen;
+
+    while (i < exp.length) {
+      console.log('loop');
+      // detect opening
+      if ( exp[i] === '(' ) {
+        openingParen = i;
+      }
+      //detect closing
+      if ( exp[i] === ')' ) {
+        closingParen = i;
+        const parenExpression = exp.slice(openingParen + 1, closingParen);
+        const parenExpressionResult = await stage(parenExpression);
+        exp.splice(openingParen, closingParen - openingParen + 1, parenExpressionResult);
+        console.log('paren resolver exp log:', exp);
+      }
+      i++;
+    }
+  }
+  const newExpression = await evaluateParens(splitExpression);
+  console.log('eval parens result:', newExpression);
+
   const result = stage(splitExpression);
   console.log('result:', result);
 }
@@ -11,11 +41,6 @@ const stage = (exp) => {
   const ops = [['*', '/'], ['+', '-']];
   // (multiplication, division) > (addition, subtraction)
   for (let op of ops) {
-    console.log('current op set:', op);
-    // let firstIndex = exp.indexOf(op[0]);
-    // let secondIndex = exp.indexOf(op[1]);
-    // console.log('first index:', firstIndex);
-    // console.log('second index:', secondIndex);
 
     const findNextOperatorIndex = (exp, firstIndex, secondIndex) => {
       if (firstIndex === secondIndex) {
@@ -32,20 +57,16 @@ const stage = (exp) => {
     let opIndex = findNextOperatorIndex(exp, exp.indexOf(op[0]), exp.indexOf(op[1]));
     // let opIndex = Math.min( exp.indexOf(op[0]), exp.indexOf(op[1]) );
     console.log('opIndex:', opIndex);
-
     let currentOp = exp[opIndex];
     console.log('currentOp:', currentOp);
 
     while (opIndex !== -1) {
       console.log('LOOP RUNS')
       exp = search(exp, currentOp, opIndex);
-      // opIndex = Math.max( exp.indexOf(op[0]), exp.indexOf(op[1]) );
-      // findNextOperator(exp);
       console.log('mutated exp:', exp);
       // reassign opIndex
-      // opIndex = Math.min( exp.indexOf(op[0]), exp.indexOf(op[1]) );
       opIndex = findNextOperatorIndex(exp, exp.indexOf(op[0]), exp.indexOf(op[1]));
-      console.log('opIndex loop:', opIndex)
+      // console.log('opIndex loop:', opIndex);
       currentOp = exp[opIndex];
     }
   }
@@ -56,7 +77,7 @@ const search = (exp, op, opIndex) => {
   let firstVal = Number(exp[opIndex-1]);
   let secondVal = Number(exp[opIndex+1]);
   // console.log('search log:', firstVal, secondVal);
-  // change exp in place—-replace expression tranche with eval
+  // change exp in place—-replace expression tranche with evaluation
   const trancheResult = solve(firstVal, secondVal, op);
   exp.splice(opIndex - 1, 3, trancheResult);
   return exp;

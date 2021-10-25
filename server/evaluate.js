b07cb4c3d570d1514008974ca9bbd9b099e92c0c
Christopher Raffaele <alumnye@gmail.com>
2021-10-24 23:12:33 -0400

    implemented for multiplication with parens

--- server/evaluate.js
+++ server/evaluate.js
@@ -1,42 +1,24 @@
 const evaluate = (expression) => {
-  // const splitExpression = expression.split(/[^0-9\.]+/g);
-  let newExpression;
+  console.log('expression---------->', expression);
 
+// if this is our first interaction with the expression, it will be a string that we first need
+  // to delimit along the operators (and parens), but keeping the operators in the resulting array.
+  // otherwise, if the expression is already an array, we leave it alone.
+  let newExpression;
   if (!Array.isArray(expression)) {
     newExpression = expression.split(/(?=[+\-/*()])|(?<=[+\-/*()])/g);
   } else {
     newExpression = expression;
   }
 
-  // another function to check parens called here
-  const evaluateParens = (firstOpenIndex, exp) => {
-    let openParenIndex;
-    for (let i = firstOpenIndex; i < exp.length; i++) {
-      let current = exp[i];
-      console.log('paren loop log:', exp[i]);
-      if (current === '(' ) {
-        openParenIndex = i;
-      } else if (current === ')') {
-        const res = evaluate(exp.slice(openParenIndex + 1, i));
-        let newExp = spliceExpression(exp, openParenIndex, i, res);
-        console.log('newExp:', newExp);
-        return newExp;
-      }
-    }
-  }
-
-  const spliceExpression = (exp, from, end, newVal) => {
-    return exp.slice(0, from).concat(newVal, exp.slice(end + 1));
-  }
-
+  // if there are parens in the expression, pass them to our parser function,
+  // which will slice out the paren expressions left-to-right and pass them back through here for evaluation.
+  // these tranches will be replaced in our larger expression with their resulting evaluations.
   const parenStart = newExpression.indexOf('(');
   if (parenStart !== -1) {
     newExpression = evaluateParens(parenStart, newExpression);
-    console.log('expressionWithoutParens:', newExpression);
     return evaluate(newExpression);
   }
-
-  ////// NO PARENS BEYOND THIS POINT
 
   const result = stage(newExpression);
   console.log('result:', Number(result));
@@ -44,12 +26,10 @@ const evaluate = (expression) => {
 }
 
 const stage = (exp) => {
-  console.log('stage log', exp);
-
   const ops = [['*', '/'], ['+', '-']];
-  // (multiplication, division) > (addition, subtraction)
+  // handle all (multiplication, division) > then (addition, subtraction)
   for (let op of ops) {
-
+    console.log('for loop');
     const findNextOperatorIndex = (exp, firstIndex, secondIndex) => {
       if (firstIndex === secondIndex) {
         return -1;
@@ -63,31 +43,52 @@ const stage = (exp) => {
     let currentOp = exp[opIndex];
 
     while (opIndex !== -1) {
+      console.log('while loop');
       exp = search(exp, currentOp, opIndex);
       // reassign opIndex
       opIndex = findNextOperatorIndex(exp, exp.indexOf(op[0]), exp.indexOf(op[1]));
       currentOp = exp[opIndex];
     }
   }
-  return exp[0];
+  // since any items left in this array will have been evaluated to integers and stripped of parens,
+  // reducing the remaining items with multiplication will handle * functionality for parens.
+  return exp.reduce((a, b) => a * b);
 }
 
+// put away
 const search = (exp, op, opIndex) => {
   let firstVal = Number(exp[opIndex-1]);
   let secondVal = Number(exp[opIndex+1]);
-  // change exp in place—-replace expression tranche with evaluation
+  // replace expression tranche with evaluation.
   const trancheResult = solve(firstVal, secondVal, op);
   exp.splice(opIndex - 1, 3, trancheResult);
   return exp;
 }
 
-const solve = (firstVal, secondVal, op) => {
-  console.log('solving this:', firstVal, op, secondVal);
-  switch(op) {
+// put away
+const solve = (firstVal, secondVal, operator) => {
+  switch(operator) {
     case '*' : return firstVal * secondVal;
     case '/' : return firstVal / secondVal;
     case '+' : return firstVal + secondVal;
     case '-' : return firstVal - secondVal;
+  }
+}
+
+const evaluateParens = (firstOpenIndex, exp) => {
+  let openParenIndex;
+  let newExpression = exp;
+  let i = firstOpenIndex;
+  while (i < exp.length) {
+    let current = exp[i];
+    if (current === '(' ) {
+      openParenIndex = i;
+    } else if (current === ')') {
+      const sliceResult = evaluate(inParen);
+      newExpression.splice(openParenIndex, i - openParenIndex + 1, sliceResult);
+      return newExpression;
+    }
+    i++;
   }
 }
 
---
+++ server/tester.js
@@ -0,0 +1,8 @@
+
+const tester = () => {
+  let num = '9'
+  console.log(isNaN( Number("9") ))
+
+
+}
+const result = tester();
